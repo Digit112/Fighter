143c1d45d59a750b16fac386e12a04fc44128103
Digit112 <astrix112@yahoo.com>
2022-02-11 11:33:32 -0500

    Added naive netcode to test.py
    Current netcode waits for the remote player's inputs before simulating the next frame. This is very bad, and the input sampling has been reduced to 15Hz to mitigate network lag.

--- test.py
+++ test.py
@@ -1,6 +1,81 @@
 import sys
 import pygame as pg
+import socket as sk
 from fighter import *
+
+# Start or connect to server.
+def p_help():
+	print("Usage:")
+	print("To start a server:")
+	print("py main.py")
+	print("py main.py <port>")
+	print("")
+	print("To connect to a server:")
+	print("py main.py <ip> <port>")
+
+port = 42069
+if len(sys.argv) == 1:
+	print("Starting server on default port %d..." % port)
+	ip = "127.0.0.1"
+	iam = "server"
+elif len(sys.argv) == 2:
+	try:
+		port = int(sys.argv[1])
+	except ValueError:
+		print("Provided port \"%s\" is not a number." % sys.argv[1])
+		p_help()
+		exit()
+
+	if port < 0 or port > 65535:
+		print("Provided port \"%d\" is not valid. (Must be between 0 and 65535, inclusive)" % port)
+		p_help()
+		exit()
+
+	print("Starting server on port %d..." % port)
+	ip = "127.0.0.1"
+	iam = "server"
+elif len(sys.argv) == 3:
+	ip = sys.argv[1]
+	try:
+		sk.inet_aton(ip)
+	except sk.error:
+		print("Provided ip \"%s\" is not valid." % sys.argv[1])
+		p_help()
+		exit()
+	
+	try:
+		port = int(sys.argv[2])
+	except ValueError:
+		print("Provided port \"%s\" is not a number." % sys.argv[2])
+		p_help()
+		exit()
+
+	if port < 0 or port > 65535:
+		print("Provided port \"%d\" is not valid. (Must be between 0 and 65535, inclusive)" % port)
+		p_help()
+		exit()
+	
+	print("Connecting to server at %s:%d..." % (sys.argv[1], port))
+	iam = "client"
+
+# Create or Connect to server
+if iam == "server":
+	s = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+	s.bind((ip, port))
+	s.listen()
+	conn, addr = s.accept()
+elif iam == "client":
+	s = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+	s.connect((ip, port))
+	conn = s
+else:
+	print("What?")
+	print("No.")
+	exit()
+
+#conn.setblocking(0)
+
+print("Connection Formed.")
 
 pg.init()
 
@@ -58,26 +133,43 @@ cam = camera((-width//2, -height//2, width, height), s)
 
 clk = pg.time.Clock()
 
+fn = 0
 while True:
+	fn += 1
+
 	clk.tick(30)
 
 	for event in pg.event.get():
 		if event.type == pg.QUIT:
+			conn.close()
 			sys.exit()
-	
-	k = pg.key.get_pressed()
 
-	f.fighters[0].set_control(fighter.JUMP, k[pg.K_UP])
-	f.fighters[0].set_control(fighter.DOWN, k[pg.K_DOWN])
-	f.fighters[0].set_control(fighter.LEFT, k[pg.K_LEFT])
-	f.fighters[0].set_control(fighter.RIGHT, k[pg.K_RIGHT])
-	f.fighters[0].set_control(fighter.BASIC, k[pg.K_a])
+	if fn % 2 == 0:
+		k = pg.key.get_pressed()
+
+		k_e = [False]*5
+		k_e[fighter.JUMP]  = k[pg.K_UP]
+		k_e[fighter.DOWN]  = k[pg.K_DOWN]
+		k_e[fighter.LEFT]  = k[pg.K_LEFT]
+		k_e[fighter.RIGHT] = k[pg.K_RIGHT]
+		k_e[fighter.BASIC] = k[pg.K_a]
+
+		conn.send(bytes(k_e))
+
+#		try:
+		l_e = list(conn.recv(5))
+#		except BlockingIOError:
+#			l_e = None
+		print(l_e)
+
+		for i in range(len(k_e)):
+			f.fighters[0].set_control(i, k_e[i])
+			f.fighters[1].set_control(i, l_e[i])
 
 	f.update()
-
-	print(f.fighters[1].health)
 
 	s.fill((0, 0, 0))
 	cam.set_target_from_fight(f)
 	cam.render(f, debug=True)
 	pg.display.flip()
+
